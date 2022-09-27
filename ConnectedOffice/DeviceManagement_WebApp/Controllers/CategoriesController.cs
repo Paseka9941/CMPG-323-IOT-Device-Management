96626fbcf66b0bde057d4080deff423eeaec852f
“[Paseka]” <110164074+Paseka9941@users.noreply.github.com>
2022-09-27 14:37:14 +0200

    initial commit

--- ConnectedOffice/DeviceManagement_WebApp/Controllers/CategoriesController.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Controllers/CategoriesController.cs
@@ -7,23 +7,33 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DeviceManagement_WebApp.Data;
 using DeviceManagement_WebApp.Models;
+using DeviceManagement_WebApp.Repository;
 
 namespace DeviceManagement_WebApp.Controllers
 {
     public class CategoriesController : Controller
     {
         private readonly ConnectedOfficeContext _context;
-
-        public CategoriesController(ConnectedOfficeContext context)
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoriesController(ICategoryRepository categoryRepository, ConnectedOfficeContext context)
         {
+            _categoryRepository = categoryRepository;
             _context = context;
         }
 
-        // GET: Categories
+        // GET: Services
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Category.ToListAsync());
+            return View(_categoryRepository.GetAll());
         }
+
+
+
+       
+
+
+        // GET: Categories
+
 
         // GET: Categories/Details/5
         public async Task<IActionResult> Details(Guid? id)
@@ -33,8 +43,7 @@ namespace DeviceManagement_WebApp.Controllers
                 return NotFound();
             }
 
-            var category = await _context.Category
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
+            var category = _categoryRepository.GetById(id);
             if (category == null)
             {
                 return NotFound();
@@ -57,9 +66,10 @@ namespace DeviceManagement_WebApp.Controllers
         public async Task<IActionResult> Create([Bind("CategoryId,CategoryName,CategoryDescription,DateCreated")] Category category)
         {
             category.CategoryId = Guid.NewGuid();
-            _context.Add(category);
-            await _context.SaveChangesAsync();
+            _categoryRepository.Add(category);
+
             return RedirectToAction(nameof(Index));
+
         }
 
         // GET: Categories/Edit/5
